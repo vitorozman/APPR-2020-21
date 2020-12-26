c564c7f5dafd304f2c6670b52a82b08ecad65d2e
Vito Rozman <rozman.vito@gmali.com>
2020-12-26 12:16:03 +0100

    Skrajsal kodo za uvoz

--- uvoz/uvoz.r
+++ uvoz/uvoz.r
@@ -5,11 +5,6 @@ library(rjson)
 
 #uvoz json datoteke
 data20 <- fromJSON(file="podatki/billionaires20.json")
-data19 <- fromJSON(file="podatki/billionaires19.json")
-data18 <- fromJSON(file="podatki/billionaires18.json")
-data17 <- fromJSON(file="podatki/billionaires17.json")
-data16 <- fromJSON(file="podatki/billionaires16.json")
-data15 <- fromJSON(file="podatki/billionaires15.json")
 
 #Glavna tabela za leto 2020
 
@@ -25,73 +20,25 @@ Tabela20 <- lapply(data20$personList$personsLists,
                           origin="1970-01-01")) %>%
                     .[1:200,] # prvih 200
 
-# Tabela za vejo BIO
-tabelaBio <- Tabela20 %>% select(ImePriimek, Rojstvo, Spol, Premozenje)
-
-# Tabela za vejo GEO
-tabelaGeo <- Tabela20 %>% select(ImePriimek, Drzava, Premozenje)
-
-# Tabela za vejo PANG
-tabelaPang <- Tabela20 %>% select(ImePriimek, Kategorija, Premozenje)                  
-
-# Tabela za vejo TOP5
-tabelaTOP <- Tabela20 %>% select(ImePriimek, Premozenje, Vir)
-
 
 stolpci_json <- c("personName", "finalWorth", "source")
 stolpci_new <- c("ImePriimek", "Premozenje", "Vir")
 
-# 2015
-Tabela15 <- lapply(data15$personList$personsLists,
-                   . %>% .[stolpci_json] %>% setNames(stolpci_new) %>% # poskrbimo za manjkajoče vrednosti
-                     lapply(. %>% { ifelse(is.null(.), NA, .) }) %>% # - te predstavimo z NA
-                     as.data.frame()) %>% 
-                    bind_rows() %>%
-                    .[1:200,]
-
-# 2016
-Tabela16 <- lapply(data16$personList$personsLists,
-                   . %>% .[stolpci_json] %>% setNames(stolpci_new) %>% # poskrbimo za manjkajoče vrednosti
-                     lapply(. %>% { ifelse(is.null(.), NA, .) }) %>% # - te predstavimo z NA
-                     as.data.frame()) %>% 
-                    bind_rows() %>%
-                    .[1:200,]
-
-# 2017
-Tabela17 <- lapply(data17$personList$personsLists,
-                   . %>% .[stolpci_json] %>% setNames(stolpci_new) %>% # poskrbimo za manjkajoče vrednosti
-                     lapply(. %>% { ifelse(is.null(.), NA, .) }) %>% # - te predstavimo z NA
-                     as.data.frame()) %>% 
-                    bind_rows() %>%
-                    .[1:200,]
-
-# 2018
-Tabela18 <- lapply(data18$personList$personsLists,
-                   . %>% .[stolpci_json] %>% setNames(stolpci_new) %>% # poskrbimo za manjkajoče vrednosti
-                     lapply(. %>% { ifelse(is.null(.), NA, .) }) %>% # - te predstavimo z NA
-                     as.data.frame()) %>%
-                    bind_rows() %>%
-                    .[1:200,]
-
-# 2019
-Tabela19 <- lapply(data19$personList$personsLists,
-                   . %>% .[stolpci_json] %>% setNames(stolpci_new) %>% # poskrbimo za manjkajoče vrednosti
-                     lapply(. %>% { ifelse(is.null(.), NA, .) }) %>% # - te predstavimo z NA
-                     as.data.frame()) %>% 
-                    bind_rows() %>%
-                    .[1:200,]
 
 
-# Shanjevanje v CSV datoteko
+leta <- 15:19 # leta, za katere obdelujemo podatke
 
-tabelaBio %>% write.csv2("podatki/TabelaBIO.csv", fileEncoding = "utf8")
-tabelaGeo %>% write.csv2("podatki/TabelaGEO.csv", fileEncoding = "utf8")
-tabelaPang %>% write.csv2("podatki/TabelaPANG.csv", fileEncoding = "utf8")
-Tabela15 %>% write.csv2("podatki/Tabela_2015.csv", fileEncoding = "utf8")
-Tabela16 %>% write.csv2("podatki/Tabela_2016.csv", fileEncoding = "utf8")
-Tabela17 %>% write.csv2("podatki/Tabela_2017.csv", fileEncoding = "utf8")
-Tabela18 %>% write.csv2("podatki/Tabela_2018.csv", fileEncoding = "utf8")
-Tabela19 %>% write.csv2("podatki/Tabela_2019.csv", fileEncoding = "utf8")
-tabelaTOP %>% write.csv2("podatki/Tabela_TOP_2020.csv", fileEncoding = "utf8")
+Tabela <- lapply(leta, function(leto)
+  sprintf("podatki/billionaires%d.json", leto) %>% # sestavimo ime datoteke
+    fromJSON(file=.) %>%
+    .$personList %>% .$personsLists %>%
+    lapply(. %>% .[stolpci_json] %>% setNames(stolpci_new) %>%
+             lapply(. %>% { ifelse(is.null(.), NA, .) }) %>%
+             as.data.frame()) %>% bind_rows() %>% .[1:200, ] %>%
+    mutate(Leto=2000+leto)) %>% bind_rows() %>%
+  rbind(Tabela20 %>% transmute(ImePriimek, Premozenje, Vir, Leto=2020)) # dodani še ustrezni stolpci za 2020
 
+# zapis tabel v csv datoteko
+Tabela20 %>% write.csv2("podatki/Tabela20.csv", fileEncoding = "utf8")
+Tabela %>% write.csv2("podatki/Tabela.csv", fileEncoding = "utf8")
 
