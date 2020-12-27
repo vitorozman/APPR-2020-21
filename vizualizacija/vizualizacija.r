ad98e121f21da33f74bc45abbbd670345e263bed
Vito Rozman <rozman.vito@gmali.com>
2020-12-27 22:18:43 +0100

    Urajanje in priprava za prikaz podatkov kategorije BIO

--- vizualizacija/vizualizacija.r
+++ vizualizacija/vizualizacija.r
@@ -1,15 +1,91 @@
-# 3. faza: Vizualizacija podatkov
+library(dplyr)
+library(readr)
+library(tidyr)
+library(ggplot2)
+library(data.table)
 
-# Uvozimo zemljevid.
-zemljevid <- uvozi.zemljevid("http://baza.fmf.uni-lj.si/OB.zip", "OB",
-                             pot.zemljevida="OB", encoding="Windows-1250")
-# Če zemljevid nima nastavljene projekcije, jo ročno določimo
-proj4string(zemljevid) <- CRS("+proj=utm +zone=10+datum=WGS84")
+# funkcija ki pretvori datum rojstva v starost osebe
+datum_v_starost <- function(datum_rojstva, trenutni_datum){
+  starost <- year(trenutni_datum) - year(datum_rojstva) - 1
+  docakal_rd <- 0
+  if (month(trenutni_datum) > month(datum_rojstva))
+    docakal_rd <- 1
+  if (month(trenutni_datum) == month(datum_rojstva) & mday(trenutni_datum) >= mday(datum_rojstva))
+    docakal_rd <- 1
+  starost <- starost + docakal_rd
+  return(starost)
+}
 
-levels(zemljevid$OB_UIME) <- levels(zemljevid$OB_UIME) %>%
-  { gsub("Slovenskih", "Slov.", .) } %>% { gsub("-", " - ", .) }
-zemljevid$OB_UIME <- factor(zemljevid$OB_UIME, levels=levels(obcine$obcina))
+Tabela20 <- read_csv2("podatki/Tabela20.csv", locale=locale(encoding="Windows-1250"))
+TabelaTOP <- read_csv2("podatki/Tabela.csv", locale=locale(encoding="Windows-1250"))
 
-# Izračunamo povprečno velikost družine
-povprecja <- druzine %>% group_by(obcina) %>%
-  summarise(povprecje=sum(velikost.druzine * stevilo.druzin) / sum(stevilo.druzin))
+tabelaBIO <- Tabela20 %>% select(ImePriimek, Rojstvo, Spol, Premozenje) %>%
+  mutate(Starost =  datum_v_starost(Rojstvo, Sys.Date()))
+  
+
+# BIO
+
+# izris povprecnega premozenja za dolocneo starost
+povprecje <- tabelaBIO %>% group_by(Starost) %>%
+  summarise(PovprecnoPremozenje = median(Premozenje, na.rm = FALSE))
+
+ggBio1 <- ggplot(data=povprecje, aes(x=Starost, y=PovprecnoPremozenje)) + 
+  geom_bar(stat = "identity", color="green") +
+  geom_point(color="red")
+ggBio1
+
+# delez moskih in zensk
+delez <- tabelaBIO %>% count(Spol)
+
+ggBio2 <- ggplot(data=tabelaBIO, aes(x="", y=Spol, fill=Spol)) + 
+  geom_bar(width = 1, stat = "identity") +
+  coord_polar("y", start=0) + 
+  scale_fill_manual(values=c("#999999", "#E69F00", "#56B4E9"), 
+                    breaks=c("F", "M", "NA"),
+                    labels=c("Ženske", "Moški", "Drugo")) +
+  labs(title="Delež moških in žensk") + 
+  xlab("") + 
+  ylab("")
+ggBio2
+
+
+# tabela odstopanj
+M_max_premozenje <- tabelaBIO[tabelaBIO$Spol == "M",] %>% slice_max(Premozenje)
+M_max_starost <- tabelaBIO[tabelaBIO$Spol == "M",] %>% slice_max(Starost)
+M_min_starost <- tabelaBIO[tabelaBIO$Spol == "M",] %>% slice_min(Starost)
+M_mean <- tabelaBIO[tabelaBIO$Spol == "M",] %>% summarise(PvprecnaStarost = mean(Starost, na.rm = TRUE))
+
+F_max_premozenje <- tabelaBIO[tabelaBIO$Spol == "F",] %>% slice_max(Premozenje)
+F_max_starost <- tabelaBIO[tabelaBIO$Spol == "F",] %>% slice_max(Starost)
+F_min_starost <- tabelaBIO[tabelaBIO$Spol == "F",] %>% slice_min(Starost)
+F_mean <- tabelaBIO[tabelaBIO$Spol == "F",] %>% summarise(PvprecnaStarost = mean(Starost, na.rm = TRUE))
+
+#moski
+Odstopanja_M <- rbind(M_max_premozenje,
+                           M_max_starost,
+                           M_min_starost) %>% mutate(M_mean)
+Odstopanja_M
+
+#zenske
+Odstopanja_F <- rbind(F_max_premozenje,
+                      F_max_starost,
+                      F_min_starost) %>% mutate(F_mean)
+Odstopanja_F
+### potrebno je se zdruziti tabele...
+
+# PANG
+
+tabelaPANG <- Tabela20 %>% select(ImePriimek, Kategorija, Premozenje)
+
+
+
+
+
+
+
+
+
+
+
+
+
