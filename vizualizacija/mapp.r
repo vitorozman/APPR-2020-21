5051d99e8240fee1f06a369f78b19ee1fa1a5bbf
Vito Rozman <rozman.vito@gmali.com>
2021-01-05 23:51:17 +0100

    Dodal zemljevid

--- vizualizacija/mapp.r
+++ vizualizacija/mapp.r
@@ -3,11 +3,8 @@ library(dplyr)
 library(readr)
 library(tidyr)
 
-
-
 GEO <- read_csv2("podatki/Tabela20.csv", locale=locale(encoding="Windows-1250")) %>% 
   select(ImePriimek, Drzava, Premozenje, -X1)
-
 
 geo_vrednosti <- GEO %>% group_by(Drzava) %>% 
   summarise(Vsota_premozenja = sum(Premozenje), Stevilo_oseb = n_distinct(ImePriimek)) 
@@ -16,7 +13,7 @@ naj_bogatas <- GEO %>% group_by(Drzava) %>% slice_max(Premozenje, n=1)
 
 geo_vrednosti <- left_join(geo_vrednosti, naj_bogatas, by="Drzava") 
 
-#osnutek za zemljevid
+# zemlevid sveta ki prikazuje vsoto skupnega premozenja po drzavah in najbogatejse za posamezno drzavo
 data("World")
 tmap_mode("view")
 svet <- tm_shape(merge(World, geo_vrednosti %>% filter(Drzava != "United States"),
@@ -26,5 +23,4 @@ svet <- tm_shape(merge(World, geo_vrednosti %>% filter(Drzava != "United States"
   tm_view(text.size.variable = TRUE) +
   tm_shape(merge(World, geo_vrednosti %>% filter(Drzava == "United States"),
                  by.x = "name", by.y = "Drzava" )) + tm_polygons(col="red3") +
-  tm_text("ImePriimek", size = 0.8) 
-svet
+  tm_text("ImePriimek", size = 0.8)
