ba464fe9e3aa1c8a58fd84714380f6f384301317
Vito Rozman <rozman.vito@gmali.com>
2021-01-05 23:45:12 +0100

    Dodal zemljevid

--- vizualizacija/mapp.r
+++ vizualizacija/mapp.r
@@ -6,22 +6,25 @@ library(tidyr)
 
 
 GEO <- read_csv2("podatki/Tabela20.csv", locale=locale(encoding="Windows-1250")) %>% 
-  select(ImePriimek, Drzava, Premozenje)
-
+  select(ImePriimek, Drzava, Premozenje, -X1)
 
 
 geo_vrednosti <- GEO %>% group_by(Drzava) %>% 
-  summarise(vsota = sum(Premozenje), stevilo = n_distinct(ImePriimek)) 
+  summarise(Vsota_premozenja = sum(Premozenje), Stevilo_oseb = n_distinct(ImePriimek)) 
 
 naj_bogatas <- GEO %>% group_by(Drzava) %>% slice_max(Premozenje, n=1)
 
 geo_vrednosti <- left_join(geo_vrednosti, naj_bogatas, by="Drzava") 
 
-
-
 #osnutek za zemljevid
 data("World")
 tmap_mode("view")
-svet <- tm_shape(merge(World, geo_vrednosti, by.x = "name", by.y = "Drzava" )) + tm_polygons("vsota")
+svet <- tm_shape(merge(World, geo_vrednosti %>% filter(Drzava != "United States"),
+                       by.x = "name", by.y = "Drzava" )) + 
+  tm_polygons("Vsota_premozenja") +
+  tm_text("ImePriimek", size = 0.8) +
+  tm_view(text.size.variable = TRUE) +
+  tm_shape(merge(World, geo_vrednosti %>% filter(Drzava == "United States"),
+                 by.x = "name", by.y = "Drzava" )) + tm_polygons(col="red3") +
+  tm_text("ImePriimek", size = 0.8) 
 svet
-
