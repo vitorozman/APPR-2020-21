93739bd1fb1341575a84f1f23a88a4d2a14b7b1d
Vito Rozman <rozman.vito@gmali.com>
2021-01-06 23:55:22 +0100

    Dodal potrebne pakete

--- lib/libraries.r
+++ lib/libraries.r
@@ -1,11 +1,18 @@
+#paketi
+
 library(knitr)
 library(rvest)
 library(gsubfn)
 library(tidyr)
 library(tmap)
 library(shiny)
+library(shinythemes)
+library(ggplot2)
+library(data.table)
+library(scales)
+library(readr)
+
 
 options(gsubfn.engine="R")
 
-# Uvozimo funkcije za pobiranje in uvoz zemljevida.
-source("lib/uvozi.zemljevid.r", encoding="UTF-8")
+
