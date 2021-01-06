9aa2eedc1a15fd5480207e73a50ed14772f86712
Vito Rozman <rozman.vito@gmali.com>
2021-01-06 23:57:19 +0100

    Dodal testni shiny app

--- shiny/server.R
+++ shiny/server.R
@@ -1,24 +1,79 @@
-library(shiny)
+# shiny SERVER
+
+
 
 shinyServer(function(input, output) {
-  output$druzine <- DT::renderDataTable({
-    druzine %>% pivot_wider(names_from="velikost.druzine", values_from="stevilo.druzin") %>%
-      rename(`Občina`=obcina)
+  output$tocke <- renderPlot({
+    if(input$Spol %in% c("M", "F")){
+      podatki <- Tabela20 %>% filter(Spol == input$Spol)
+    }
+    else {
+      podatki <- Tabela20
+    }
+    if (input$ext == "Max"){
+      podatki <- podatki %>% group_by(Kategorija) %>% summarise(Odmik = max(Premozenje))
+    }
+    else if (input$ext == "Min"){
+      podatki <- podatki %>% group_by(Kategorija) %>% summarise(Odmik = min(Premozenje))
+    }
+    else {
+      podatki <- podatki %>% group_by(Kategorija) %>% summarise(Odmik = mean(Premozenje))
+    }
+    #podatki <- podatki %>% filter((input$age[1] <= podatki$Starost) & (podatki$Starost <= input$age[2]))
+    print(ggplot(podatki) +
+            aes(x = Kategorija, y = Odmik, col = Kategorija)+
+            geom_point(size = 3, na.rm = TRUE) +
+            xlab("Starost") +
+            ylab("Premozenje (mio €)") +
+            labs(title="Premoženje oseb ob določeni starosti") +
+            scale_y_continuous(breaks = seq(7000, 150000, by=15000), limits = c(0, 150000)) +
+            theme(axis.text.x = element_blank(),
+                  axis.title = element_blank())+
+            xlab("Panoge") + 
+            ylab("mio €") +
+            guides(col=guide_legend("Panoge"))
+            
+          )
+  })
+  output$graf <- renderPlot({
+    podatki_oseba <- filter(TabelaTOP, ImePriimek %in% input$bogatas)
+    print(ggplot(podatki_oseba) +
+            aes(x=Leto, y=Premozenje) +
+            geom_line(col="red3", size=1) + 
+            geom_point() +
+            scale_y_continuous(breaks = seq(5000, 150000, by=25000),limits = c(5000, 150000)) +
+            theme(axis.ticks = element_line(color = "red3"),
+                  axis.line = element_line(colour = "black", 
+                                           size = 1, linetype = "solid"),
+                  axis.ticks.length = unit(2, "mm"),
+                  legend.position = "none")
+          )
+    
   })
   
-  output$pokrajine <- renderUI(
-    selectInput("pokrajina", label="Izberi pokrajino",
-                choices=c("Vse", levels(obcine$pokrajina)))
-  )
-  output$naselja <- renderPlot({
-    main <- "Pogostost števila naselij"
-    if (!is.null(input$pokrajina) && input$pokrajina %in% levels(obcine$pokrajina)) {
-      t <- obcine %>% filter(pokrajina == input$pokrajina)
-      main <- paste(main, "v regiji", input$pokrajina)
-    } else {
-      t <- obcine
-    }
-    ggplot(t, aes(x=naselja)) + geom_histogram() +
-      ggtitle(main) + xlab("Število naselij") + ylab("Število občin")
-  })
 })
+
+
+
+#shinyServer(function(input, output) {
+#  output$druzine <- DT::renderDataTable({
+#    druzine %>% pivot_wider(names_from="velikost.druzine", values_from="stevilo.druzin") %>%
+#      rename(`Občina`=obcina)
+#  })
+#  
+#  output$pokrajine <- renderUI(
+#    selectInput("pokrajina", label="Izberi pokrajino",
+#                choices=c("Vse", levels(obcine$pokrajina)))
+#  )
+#  output$naselja <- renderPlot({
+#    main <- "Pogostost števila naselij"
+#    if (!is.null(input$pokrajina) && input$pokrajina %in% levels(obcine$pokrajina)) {
+#      t <- obcine %>% filter(pokrajina == input$pokrajina)
+#      main <- paste(main, "v regiji", input$pokrajina)
+#    } else {
+#      t <- obcine
+#    }
+#    ggplot(t, aes(x=naselja)) + geom_histogram() +
+#      ggtitle(main) + xlab("Število naselij") + ylab("Število občin")
+#  })
+#})
--- shiny/ui.R
+++ shiny/ui.R
@@ -1,17 +1,53 @@
-library(shiny)
+# shiny UI
 
-shinyUI(fluidPage(
-  
-  titlePanel("Slovenske občine"),
-  
-  tabsetPanel(
-      tabPanel("Velikost družine",
-               DT::dataTableOutput("druzine")),
-      
-      tabPanel("Število naselij",
-               sidebarPanel(
-                  uiOutput("pokrajine")
-                ),
-               mainPanel(plotOutput("naselja")))
-    )
-))
+drop_list <- c("M", "F", "Family" )
+
+
+shinyUI(
+  fluidPage(
+    navbarPage("Najbogatejši ljudje na svetu 2020",
+            tabPanel("Panoge",
+                     titlePanel(title = h2("Prikaz premoženj", align="center")),
+                        sidebarLayout(
+                        sidebarPanel(
+                          selectInput(inputId = "Spol",
+                                      label = "Spol",
+                                      choices = c( "-", "M", "F")),
+                          selectInput(inputId = "ext",
+                                      label = "Ekstrem premoženja",
+                                      choices = c("Max", "Min", "Mean"),),
+                          sliderInput(inputId = "age",
+                                      label = "Dolžina intervala starosti",
+                                      min = 20, 
+                                      max = 100,
+                                      value = c(20, 100)),
+                          checkboxGroupInput("variable", "Variables to show:",
+                                             c("C1" = "c1",
+                                               "C2" = "c2",
+                                               "C2" = "c2")),
+                          checkboxInput("somevalue", "Some value", FALSE)
+                        ), #konec 1. sidebar Panel
+                        
+                        mainPanel(
+                          plotOutput(outputId = "tocke"))
+                        
+                      )
+                    ), #konec 1. tab Panel
+            tabPanel("Sprememba premoženja",
+                     titlePanel(title = h2("Graf rasti premoženja", align="center")),
+                     sidebarLayout(
+                       sidebarPanel(
+                         selectInput(inputId = "bogatas",
+                                     label = "Izbera osebe",
+                                     choices = c(filter(TabelaTOP, Leto == 2020) %>% .[1:40,] %>% .$ImePriimek))
+                       ), #konec 2. sidebar Panel
+                       mainPanel(
+                         plotOutput(outputId = "graf"))
+                       
+                     )
+                    ) #konec 2. tab Panel
+    ), #konec navbarPage
+    theme = shinytheme("slate")
+  ) #konec fluid page
+) #konec shinyUI
+
