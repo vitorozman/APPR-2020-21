9183aafd5cd86ae5b5f5b1b6f5860643a29626a3
Vito Rozman <rozman.vito@gmali.com>
2021-01-10 22:50:37 +0100

    Dodal opise in moznost napovedi premozenja

--- shiny/server.R
+++ shiny/server.R
@@ -45,13 +45,42 @@ shinyServer(function(input, output) {
             aes(x=Leto, y=Premozenje) +
             geom_line(col="red3", size=1) + 
             geom_point() +
-            scale_y_continuous(breaks = seq(5000, 150000, by=25000),limits = c(5000, 150000)) +
+            scale_y_continuous(breaks = seq(5000, 155000, by=25000),limits = c(5000, 155000)) +
+            scale_x_continuous(breaks = seq(2015, 2020, by=1),limits = c(2015, 2020)) +
+            ylab("Premoženje v mio €") +
             theme(axis.ticks = element_line(color = "red3"),
                   axis.line = element_line(colour = "black", 
                                            size = 1, linetype = "solid"),
                   axis.ticks.length = unit(2, "mm"),
                   legend.position = "none")
           )
+    # Prileganje
+    if (input$lin){
+      podatki <- podatki_oseba %>%
+        ungroup %>% 
+        select(-Vir, -ImePriimek)
+      prileganje <- glm(data=podatki_oseba, Premozenje ~ Leto)
+      leta <- data.frame(Leto=seq(2015, 2021, 1))
+      napoved <- leta %>% mutate(Premozenje=predict(prileganje, leta))
+      print(ggplot(data=podatki %>% rbind(napoved %>% filter(Leto == 2021))) +
+              aes(x=Leto, y=Premozenje) +
+              geom_line(col="red3", size=1, na.rm=TRUE) +
+              geom_point(na.rm = TRUE) +
+              geom_point(data = napoved %>% filter(Leto==2021), aes(x=Leto, y=Premozenje, color="red", size=3), na.rm = TRUE) +
+              geom_text(aes(label=ifelse(Leto == 2021, round(Premozenje, -3), "")), nudge_y=10000) + # napoved rasti in premoženja
+              geom_smooth(data=podatki, method="glm", formula=y ~ x, fullrange=TRUE) +
+              scale_y_continuous(breaks = seq(5000, 155000, by=25000),limits = c(5000, 160900)) +
+              scale_x_continuous(breaks = seq(2015, 2021, by=1),limits = c(2015, 2021)) +
+              labs(title=paste("Prileganje in napoved premoženja za leto 2021", input$bogatas, sep=" - ")) +
+              ylab("Premoženje v mio €") +
+              theme(axis.ticks = element_line(color = "red3"),
+                    axis.line = element_line(colour = "black", 
+                                             size = 1, linetype = "solid"),
+                    axis.ticks.length = unit(2, "mm"),
+                    legend.position = "none")
+            )
+
+    }
     
   })
   
--- shiny/ui.R
+++ shiny/ui.R
@@ -5,43 +5,42 @@ shinyUI(
   fluidPage(
     navbarPage("200 najbogatejši ljudje na svetu",
             tabPanel("Panoge",
-                     titlePanel(title = h2("Prikaz premoženj", align="center")),
+                     titlePanel(title = h2("Prikaz premoženj glede na panogo", align="center")),
                         sidebarLayout(
                         sidebarPanel(
+                          h3("Filtriranje"),
+                          p("Točka = kolikšno je ekstrem premoženja za določeno panogo"),
                           selectInput(inputId = "Spol",
                                       label = "Spol",
                                       choices = c( "-", "M", "F")),
+                          p(tags$i("[ - ] ne filtrira glede na spol")),
                           selectInput(inputId = "ext",
                                       label = "Ekstrem premoženja",
                                       choices = c("Max", "Min", "Mean"),),
+                          p(tags$i("Izbrani ekstrem prikaže ekstrem premoženja za določemo panogo")),
                           sliderInput(inputId = "age",
                                       label = "Dolžina intervala starosti",
                                       min = 20, 
                                       max = 100,
-                                      value = c(20, 100))
-                          #checkboxGroupInput("variable", "Variables to show:",
-                          #                   c("C1" = "c1",
-                          #                     "C2" = "c2",
-                          #                     "C2" = "c2")),
-                          #checkboxInput("somevalue", "Some value", FALSE)
+                                      value = c(20, 100)),
+                          p(tags$i("Omeji analizerane osebe s starostjo iz zgornjega intervala"))
                         ), #konec 1. sidebar Panel
-                        
                         mainPanel(
                           plotOutput(outputId = "tocke"))
-                        
                       )
                     ), #konec 1. tab Panel
             tabPanel("Sprememba premoženja",
-                     titlePanel(title = h2("Graf rasti premoženja", align="center")),
+                     titlePanel(title = h2("Graf premoženja", align="center")),
                      sidebarLayout(
                        sidebarPanel(
                          selectInput(inputId = "bogatas",
                                      label = "Izbera osebe",
-                                     choices = c(filter(TabelaTOP, Leto == 2020) %>% .[1:40,] %>% .$ImePriimek))
+                                     choices = c(filter(TabelaTOP, Leto == 2020) %>% .[1:50,] %>% .$ImePriimek)),
+                         checkboxInput(inputId = "lin", "Napoved premoženja", FALSE),
+                         p(tags$i("S splošnim linearnim modelom ocenimo prileganje točkam in napovemo prihodnje premoženje za leto 2021"))
                        ), #konec 2. sidebar Panel
                        mainPanel(
                          plotOutput(outputId = "graf"))
-                       
                      )
                     ) #konec 2. tab Panel
     ), #konec navbarPage
